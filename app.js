7ab3a280e97d041cea6b1824828fee6038056827
onaefe@outlook.com <onaefe@outlook.com>
2018-05-13 10:17:38 +0100

    Reassigend display from to public folder from middleware in app.js

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
+// var logger = (req, res, next)=>{
+//     console.log('...Logging');
+//     next();
+// }
+
+// app.use(logger);
+
+//Body Parser MiddleWare
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Set Static path
+app.use(express.static(path.join(__dirname,'public')));
 
 
 app.get('/',(req, res)=>{
     res.send('Hello! valid request');
+});
+app.get('/about',(req, res)=>{
+    res.send('Hello! about request');
 });
 app.listen(4200,function(){
     console.log('server started on port 4200');
